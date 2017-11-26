0fbeb9b4e06348492f167d30bbf3bd4042239d29
Brian Eggleston <bje301@gmail.com>
2017-11-26 02:36:47 -0500

    initial commit

---
+++ src/main/java/oi/sdtwo/famtree/api/FamTreeApiApplication.java
@@ -0,0 +1,12 @@
+package oi.sdtwo.famtree.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamTreeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamTreeApiApplication.class, args);
+	}
+}
---
+++ src/test/java/oi/sdtwo/famtree/api/FamTreeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package oi.sdtwo.famtree.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FamTreeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
